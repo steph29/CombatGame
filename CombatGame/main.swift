3658575ec2353be259708501763bdca8f31979a9
steph29 <fingolfin3529@gmail.com>
2018-09-26 21:11:22 +0200

    Initial Commit

---
+++ CombatGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CombatGame
+//
+//  Created by stephane verardo on 26/09/2018.
+//  Copyright © 2018 stephane verardo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
